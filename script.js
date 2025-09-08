d6ed642517d7ac0d5700f98819b6ee2232b0b94a
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-09-07 22:03:55 -0300

    Commi weeks 36 & 37

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //actualizarDiasRestantes();
-  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  actualizarDiasRestantes();
+  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
   //Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 34',
-    data: [18, 11, 16, 20], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 36',
+    data: [13, 20, 21, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 35',
-    data: [12, 14, 10, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 37',
+    data: [12, 19, 16, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
